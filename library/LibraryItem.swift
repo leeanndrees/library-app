c05b6684bc682fab62bf75d2fc00844e7173be9c
Leeann Drees <leeann.drees@detroitlabs.com>
2018-09-04 16:31:56 -0400

    setting up basic library class

---
+++ library/LibraryItem.swift
@@ -0,0 +1,14 @@
+//
+//  LibraryItem.swift
+//  library
+//
+//  Created by DetroitLabs on 9/4/18.
+//  Copyright © 2018 DetroitLabs. All rights reserved.
+//
+
+import Foundation
+
+class LibraryItem {
+    var name = ""
+    var author = ""
+}
